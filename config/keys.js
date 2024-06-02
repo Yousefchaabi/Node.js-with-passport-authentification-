ec2c0e7894de628ee8b490a71ee705b3bd405cb7
Yousefchaabi <92028687+Yousefchaabi@users.noreply.github.com>
2024-06-03 00:04:22 +0100

    first commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword =
+  "mongodb+srv://Your_UserName_Here:PassWord_Here@cluster0.50dqo.mongodb.net/users?retryWrites=true&w=majority&appName=Cluster0";
+
+module.exports = {
+  mongoURI: dbPassword,
+};
